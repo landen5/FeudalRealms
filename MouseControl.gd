59481219543ffd5dce7fa66864a58fa7664dbdea
Landen <liveoaklanden@gmail.com>
2021-03-07 23:07:29 -0800

    updated unit selection - no longer hard coded for a specific unit

--- MouseControl.gd
+++ MouseControl.gd
@@ -1,7 +1,8 @@
 extends Spatial
 
-onready var cam = get_tree().get_root().get_node("Spatial/Camera")
-onready var tank = get_tree().get_root().get_node("Spatial/Ballista")
+onready var cam = get_tree().get_root().get_node("Base/Camera")
+onready var tank = get_tree().get_root().get_node("Base/Ballista")
+onready var unit = null
 
 var unit_pos = Vector3()
 var target_pos = Vector3()
@@ -20,17 +21,19 @@ func _process(delta):
 	var to = ray_origin + ray_direction * 1000.0
 	var space_state = get_world().get_direct_space_state()
 	var hit = space_state.intersect_ray(from, to)
-	if hit.size() != 0:
-		print(hit.collider.name)
-		if(hit.collider.name == "Ballista"):
-			if(Input.is_action_pressed("mouse_left")):
-				print("Clicked left mouse btn")
+	if(Input.is_action_just_pressed("mouse_left")):
+		if hit.size() != 0:
+			var selected_unit = hit.collider.name
+			print(selected_unit)
+			unit = get_tree().get_root().get_node("Base/" + selected_unit)
+			print(unit)
+			
 
 func _physics_process(delta):
 	if(Input.is_action_pressed("Forward")):
-		tank.apply_impulse(Vector3(0, 0, 0), Vector3.FORWARD * 5 * delta)
+		unit.apply_impulse(Vector3(0, 0, 0), Vector3.FORWARD * 5 * delta)
 	if(Input.is_action_pressed("Back")):
-		tank.apply_impulse(Vector3(0, 0, 0), Vector3.BACK * 5 * delta)
+		unit.apply_impulse(Vector3(0, 0, 0), Vector3.BACK * 5 * delta)
 				
 func toggle_fullscreen(delta):
 	if(Input.is_action_just_pressed("F")):
