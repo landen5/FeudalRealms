0bbae694f804609e758d36348582dcb4c7b77fa4
Landen <liveoaklanden@gmail.com>
2021-03-08 18:14:09 -0800

    added red destination marker

--- Ballista.gd
+++ Ballista.gd
@@ -1,8 +1,27 @@
 extends RigidBody
 
+onready var nav = get_tree().get_root().get_node("Base/Navigation")
+
+var unit = null
+var move_destination = Vector3()
+export var speed = 5
+
 func _physics_process(delta):
-	var unit = get_tree().get_root().get_node("Base/Camera/Controls").unit
+	
+	unit = get_tree().get_root().get_node("Base/Camera/Controls").unit
+	move_destination = get_tree().get_root().get_node("Base/Camera/Controls").move_destination
+	
+	if(unit != null && Input.is_action_pressed("middle_mouse")):
+		simple_move(delta)
+	
 	if(Input.is_action_pressed("Forward")):
-		unit.apply_impulse(Vector3(0, 0, 0), Vector3.FORWARD * 5 * delta)
+		unit.apply_impulse(Vector3(0, 0, 0), Vector3.FORWARD * speed * delta)
 	if(Input.is_action_pressed("Back")):
-		unit.apply_impulse(Vector3(0, 0, 0), Vector3.BACK * 5 * delta)
+		unit.apply_impulse(Vector3(0, 0, 0), Vector3.BACK * speed * delta)
+
+func simple_move(delta):
+	var dir = Vector3()
+	dir = (move_destination - unit.global_transform.origin).normalized()
+	if(move_destination.distance_to(unit.global_transform.origin) > 3):
+		unit.apply_impulse(Vector3(0, 0, 0), dir * 10 * delta)
+
--- MouseControl.gd
+++ MouseControl.gd
@@ -1,12 +1,10 @@
 extends Spatial
 
 onready var cam = get_tree().get_root().get_node("Base/Camera")
+var DebugMarker = preload("res://DestinationMarker.tscn")
 onready var unit = null
 
-var unit_pos = Vector3()
-var target_pos = Vector3()
-var dir = Vector3()
-var begin_move = false
+var move_destination = Vector3()
 
 var fullscreen_toggle = true
 
@@ -20,6 +18,9 @@ func _process(delta):
 	var to = ray_origin + ray_direction * 1000.0
 	var space_state = get_world().get_direct_space_state()
 	var hit = space_state.intersect_ray(from, to)
+	
+	var MarkerInstance = null
+	
 	if(Input.is_action_just_pressed("mouse_left")):
 		if hit.size() != 0:
 			var selected_unit = hit.collider.name
@@ -28,6 +29,16 @@ func _process(delta):
 			if(temp_selection.get_class() == "RigidBody"):
 				unit = get_tree().get_root().get_node("Base/" + selected_unit)
 			print(unit)
+	if(Input.is_action_just_pressed("middle_mouse")):
+		if hit.size() != 0:
+			move_destination = hit.position
+			MarkerInstance = DebugMarker.instance()
+			get_tree().get_root().add_child(MarkerInstance)
+			MarkerInstance.global_transform.origin = hit.position
+	if(MarkerInstance != null):
+		if(MarkerInstance.global_transform.origin.distance_to(unit.global_transform.origin) < 5):
+			MarkerInstance.queue_free()
+
 				
 func toggle_fullscreen(delta):
 	if(Input.is_action_just_pressed("F")):
